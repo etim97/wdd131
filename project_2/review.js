773468417af263106fe577b8872ad478bb046517
Etim 97 <Wisdomlds97@gmail.com>
2025-02-16 11:43:54 +0100

    week5 assignment

---
+++ project_2/review.js
@@ -0,0 +1,5 @@
+// Set current year in footer
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Set last modified date
+document.getElementById('last-modified').textContent = `Last modified: ${document.lastModified}`;
